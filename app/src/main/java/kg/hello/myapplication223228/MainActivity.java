20830bcafb6fef680a39fe9fa5a5dedde44a6f51
Sabubna <toktonalieva_sa@auca.kg>
2021-11-22 17:17:46 +0600

    midterm2

---
+++ app/src/main/java/kg/hello/myapplication223228/MainActivity.java
@@ -0,0 +1,18 @@
+package kg.hello.myapplication223228;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
